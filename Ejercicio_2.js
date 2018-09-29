1982e5ea48bf820c274cd7e3ee9c06c93310ae1f
CesarRosales16 <00060917@uca.edu.sv>
2018-09-29 00:09:07 -0600

    Arreglado

--- Ejercicio_2.js
+++ Ejercicio_2.js
@@ -1,4 +1,40 @@
 class Conversiones {
+    m_cm(num) {
+        return num * 100;
+    }
+    cm_m(num) {
+        return num / 100;
+    }
+    km_m(num) {
+        return num / 1000;
+    }
+    m_km(num) {
+        return num * 1000;
+    }
+    p_m(num) {
+        return num * 0.3048;
+    }
+    m_p(num) {
+        return num / 0.3048;
+    }
+    c_f(num) {
+        return ((9 * num) / 5) + 32;
+    }
+    f_c(num) {
+        return (5 * (num - 32)) / 9;
+    }
+    k_f(num) {
+        return ((9 * (num - 273.15)) / 5) + 32;
+    }
+    f_k(num) {
+        return ((5 * (num - 32)) / 9) + 273.15;
+    }
+    k_c(num) {
+        return num - 273.15;
+    }
+    c_k(num) {
+        return num + 273.15;
+    }
     convertir(medida, unidadEntrada, unidadSalida, tipo) {
         switch (tipo) {
             case 'L':
@@ -45,41 +81,5 @@ class Conversiones {
                 console.log("Tipo invalido");
                 break;
         }
-    }
-    m_cm(num) {
-        return num * 100;
-    }
-    cm_m(num) {
-        return num / 100;
-    }
-    km_m(num) {
-        return num / 1000;
-    }
-    m_km(num) {
-        return num * 1000;
-    }
-    p_m(num) {
-        return num * 0.3048;
-    }
-    m_p(num) {
-        return num / 0.3048;
-    }
-    c_f(num) {
-        return ((9 * num) / 5) + 32;
-    }
-    f_c(num) {
-        return (5 * (num - 32)) / 9;
-    }
-    k_f(num) {
-        return ((9 * (num - 273.15)) / 5) + 32;
-    }
-    f_k(num) {
-        return ((5 * (num - 32)) / 9) + 273.15;
-    }
-    k_c(num) {
-        return num - 273.15;
-    }
-    c_k(num) {
-        return num + 273.15;
     }
 } 

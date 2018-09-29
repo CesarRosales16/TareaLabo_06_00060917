9ba20cc938b9ee1283bd4b92b7e9a3cd361ee6fe
CesarRosales16 <00060917@uca.edu.sv>
2018-09-29 00:06:53 -0600

    Terminados

--- Ejercicio_2.js
+++ Ejercicio_2.js
@@ -2,8 +2,44 @@ class Conversiones {
     convertir(medida, unidadEntrada, unidadSalida, tipo) {
         switch (tipo) {
             case 'L':
+                if (unidadEntrada.toLowerCase() == "m" && unidadSalida.toLowerCase() == "cm") {
+                    console.log(m_cm(medida) + "cm");
+                }
+                if (unidadEntrada.toLowerCase() == "cm" && unidadSalida.toLowerCase() == "m") {
+                    console.log(cm_m(medida) + "m");
+                }
+                if (unidadEntrada.toLowerCase() == "km" && unidadSalida.toLowerCase() == "m") {
+                    console.log(km_m(medida) + "m");
+                }
+                if (unidadEntrada.toLowerCase() == "m" && unidadSalida.toLowerCase() == "km") {
+                    console.log(m_km(medida) + "km");
+                }
+                if (unidadEntrada.toLowerCase() == "p" && unidadSalida.toLowerCase() == "m") {
+                    console.log(p_m(medida) + "m");
+                }
+                if (unidadEntrada.toLowerCase() == "m" && unidadSalida.toLowerCase() == "p") {
+                    console.log(m_p(medida) + "p");
+                }
                 break;
             case 'T':
+                if (unidadEntrada.toUpperCase() == "C" && unidadSalida.toUpperCase() == "F") {
+                    console.log(c_f(medida) + "F");
+                }
+                if (unidadEntrada.toUpperCase() == "F" && unidadSalida.toUpperCase() == "C") {
+                    console.log(f_c(medida) + "C");
+                }
+                if (unidadEntrada.toUpperCase() == "K" && unidadSalida.toUpperCase() == "F") {
+                    console.log(k_f(medida) + "F");
+                }
+                if (unidadEntrada.toUpperCase() == "F" && unidadSalida.toUpperCase() == "K") {
+                    console.log(f_k(medida) + "K");
+                }
+                if (unidadEntrada.toUpperCase() == "K" && unidadSalida.toUpperCase() == "C") {
+                    console.log(k_c(medida) + "C");
+                }
+                if (unidadEntrada.toUpperCase() == "C" && unidadSalida.toUpperCase() == "K") {
+                    console.log(c_k(medida) + "K");
+                }
                 break;
             default:
                 console.log("Tipo invalido");
@@ -11,39 +47,39 @@ class Conversiones {
         }
     }
     m_cm(num) {
-        return num*100;
+        return num * 100;
     }
     cm_m(num) {
-        return num/100;
+        return num / 100;
     }
     km_m(num) {
-        return num/1000;
+        return num / 1000;
     }
     m_km(num) {
-        return num*1000;
+        return num * 1000;
     }
     p_m(num) {
-        return num*0.3048;
+        return num * 0.3048;
     }
     m_p(num) {
-        return num/0.3048;
+        return num / 0.3048;
     }
     c_f(num) {
-        return ((9*num)/5)+32;
+        return ((9 * num) / 5) + 32;
     }
     f_c(num) {
-        return (5*(num-32))/9;
+        return (5 * (num - 32)) / 9;
     }
-    k_f(num){
-        return ((9*(num-273.15))/5)+32;
+    k_f(num) {
+        return ((9 * (num - 273.15)) / 5) + 32;
     }
-    f_k(num){
-        return ((5*(num-32))/9)+273.15;
+    f_k(num) {
+        return ((5 * (num - 32)) / 9) + 273.15;
     }
     k_c(num) {
-        return num-273.15;
+        return num - 273.15;
     }
-    c_k(num){
-        return num+273.15;
+    c_k(num) {
+        return num + 273.15;
     }
 } 
